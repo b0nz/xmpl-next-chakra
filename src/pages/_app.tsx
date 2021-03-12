ca1cfe9d42415201a6c63dde1a5f84d23f110207
b0nz <elliantosarono.p@gmail.com>
2021-03-13 04:26:31 +0700

    update theme, font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
-import theme from '../theme'
 import { AppProps } from 'next/app'
+import { Global } from '@emotion/react'
+import theme from '../theme'
+
+const Fonts = () => (
+  <Global
+    styles={`
+      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap');
+    `}
+  />
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,18 +1,23 @@
 import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace`, body: `'Poppins', sans-serif` }
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
+  sm: "30em",
+  md: "48em",
+  lg: "62em",
+  xl: "80em",
+  "2xl": "96em",
 })
 
 const theme = extendTheme({
   colors: {
     black: '#16161D',
+    ikbppurple: {
+      100: "#5352A0",
+      500: "#373D7F",
+    },
   },
   fonts,
   breakpoints,
