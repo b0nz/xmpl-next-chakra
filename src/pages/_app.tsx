b3e502ffe5c32e5b4b8991a8aea9864785aa1373
b0nz <elliantosarono.p@gmail.com>
2021-03-16 18:25:14 +0700

    update base body background color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,6 +18,10 @@ const Fonts = () => (
   <Global
     styles={`
       @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;800;900&display=swap');
+
+      body {
+        background-color: #F7FAFC;
+      }
     `}
   />
 )
