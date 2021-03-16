b453a5cc0225daae8916896786e44aadf3a5891a
b0nz <elliantosarono.p@gmail.com>
2021-03-16 18:26:16 +0700

    update container, main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,19 +4,19 @@ import { EventCard } from './EventCard/EventCard'
 
 export const Main = () => {
   return (
-    <Stack direction="column" spacing={10} p="10">
+    <Stack direction="column" spacing={10} py={10}>
       <Flex w="full" direction="column" >
         <Flex justify="space-between" align="center" marginBottom="3">
-          <Text fontSize={['md', 'lg']} fontWeight="bold" >IKBP News</Text>
+          <Text fontSize={['md', 'lg']} fontWeight="bold">IKBP News</Text>
           <Link color="darkPurple.900" href="#">
             View All
           </Link>
         </Flex>
-        <Stack direction={["column", "row"]}>
+        <Grid gap={15} templateColumns={{md: "repeat(3, 1fr)"}}>
           <NewsCard />
           <NewsCard />
           <NewsCard />
-        </Stack>
+        </Grid>
       </Flex>
       <Flex w="full" direction="column" >
         <Flex justify="space-between" align="center" marginBottom="3">
@@ -25,7 +25,7 @@ export const Main = () => {
             View All
           </Link>
         </Flex>
-        <Grid templateColumns={["1fr", "repeat(2, 1fr)"]} gap={3}>
+        <Grid templateColumns={["1fr", "repeat(2, 1fr)"]} gap={15}>
           <EventCard />
           <EventCard />
           <EventCard />
