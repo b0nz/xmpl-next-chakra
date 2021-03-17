44cc43dd774aee3c539c8df7bbb12764d6c7f28a
b0nz <elliantosarono.p@gmail.com>
2021-03-17 18:16:33 +0700

    update banner

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,27 +1,49 @@
-import {
-  Flex,
-  Image,
-} from "@chakra-ui/react"
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+import Slider from '@farbenmeer/react-spring-slider'
+import { Button } from '@chakra-ui/react'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+const images = [
+	"https://images.pexels.com/photos/3740695/pexels-photo-3740695.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	"https://images.pexels.com/photos/3740446/pexels-photo-3740446.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	"https://images.pexels.com/photos/351265/pexels-photo-351265.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+	"https://images.pexels.com/photos/924675/pexels-photo-924675.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+];
+
+const imageStyle = (src: string) => ({
+	backgroundSize: "cover",
+	backgroundImage: `url(${src})`,
+	height: "100%",
+	width: "100%",
+});
+
+const customArrow = ({ onClick, direction }) => direction === "left" ? (
+  <Button onClick={onClick} bg="transparent">
+    <FiChevronLeft color="white" size={30} />
+  </Button>
+) : (
+  <Button onClick={onClick} bg="transparent">
+    <FiChevronRight color="white" size={30} />
+  </Button>
+)
 
 export const Hero = () => {
   return (
-    <Slider
-      autoplay
-      duration={5000}
-      previousButton={null}
-      nextButton={null}
-    >
-      <Flex justify="center" align="center" bg="black">
-        <Image src="https://w.wallhaven.cc/full/rd/wallhaven-rd7drw.jpg" />
-      </Flex>
-      <Flex justify="center" align="center" bg="black">
-        <Image src="https://w.wallhaven.cc/full/rd/wallhaven-rd7drw.jpg" />
-      </Flex>
-      <Flex justify="center" align="center" bg="black">
-        <Image src="https://w.wallhaven.cc/full/rd/wallhaven-rd7drw.jpg" />
-      </Flex>
-    </Slider>
+    <div style={{ width: "100%", height: "400px" }}>
+      <Slider
+        hasBullets
+        hasArrows
+        auto={5000}
+        bulletStyle={{ backgroundColor: "#fff", width: '10px', height: '10px' }}
+        ArrowComponent={customArrow}
+      >
+        {images.map((image) => (
+          <div
+            key={image}
+            draggable="false"
+            style={imageStyle(image)}
+          />
+        ))}
+      </Slider>
+    </div>
   )
 }
