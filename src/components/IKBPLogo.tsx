abf55c0d8abeca3ddc87b2fc52c92d7e95fd4575
b0nz <elliantosarono.p@gmail.com>
2021-04-22 20:51:31 +0700

    update logo size

--- src/components/IKBPLogo.tsx
+++ src/components/IKBPLogo.tsx
@@ -6,7 +6,7 @@ import NextLink from 'next/link'
 const IKBPLogo: FC<{ href: string }> = ({ href }) => (
   <NextLink href={href} passHref>
     <chakra.a display="block" aria-label="Institut Kristen Bukit Pengharapan">
-      <Image src="/IKBP.jpg" width={221} height={101} alt="ikbp" />
+      <Image src="/IKBP.jpg" width={208} height={95} alt="ikbp" />
     </chakra.a>
   </NextLink>
 )
